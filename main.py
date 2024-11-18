295b5a0a9408363acad43c0a1ba6b356e5395525
PaulYH <paulhumenny@icloud.com>
2024-11-18 17:41:54 -0500

    Implement pdf file merging

---
+++ main.py
@@ -0,0 +1,34 @@
+import argparse
+from pypdf import PdfWriter
+from os import listdir
+from os.path import isfile, join
+
+parser = argparse.ArgumentParser(description='CLI tool for PDF merging')
+parser.add_argument('files', type=str, nargs='+', help='PDF file names')
+
+raw_list = parser.parse_args().files
+file_list = []
+
+if '.' in raw_list:
+    files_in_dir = [f for f in listdir('.') if isfile(join('.', f))]
+    file_list = list(filter(lambda file : file.endswith('.pdf'), files_in_dir))
+else:
+    file_list = list(filter(lambda file : file.endswith('.pdf'), raw_list))
+
+try:
+    merger = PdfWriter()
+
+    print('MERGING:')
+    for pdf in file_list:
+        print(f"   {pdf}")
+        merger.append(pdf)
+
+    merger.write('combinepdf.pdf')
+    merger.close()
+except Exception as e:
+    merger.close()
+    print(f"ERROR: {e}")
+
+parser.exit("\nFiles merged successfully!")
+
+exit(code=0)
